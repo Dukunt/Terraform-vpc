09c7e3a495332e97d37c5cf2d55ba5d3b8aff3f3
sateesh <sateesh1417@gmail.com>
2026-03-06 21:22:01 +0000

    az

--- variables.tf
+++ variables.tf
@@ -19,3 +19,14 @@ variable "ig_tags" {
     type = map
     default = {}
 }
+
+variable public_subnet_cidrs {
+    type = list
+    default = ["10.0.1.0/24","10.0.2.0/24"]
+
+}
+
+variable public_subnet_tags {
+    type = map
+    default = {}
+}
