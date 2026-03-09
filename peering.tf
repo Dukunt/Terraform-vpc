f5808bc3c8ad2b8cba64d74b4cf35184bed148e2
sateesh <sateesh1417@gmail.com>
2026-03-09 20:18:13 +0000

    az

--- peering.tf
+++ peering.tf
@@ -22,3 +22,17 @@ resource "aws_vpc_peering_connection" "dev_def" {
   )
 
 }
+
+resource "aws_route" "public_peer" {
+  count = var.is_peerring_required ? 1 : 0
+  route_table_id            = aws_route_table.public_rt.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.dev_def[count.index].id
+}
+
+resource "aws_route" "default_def" {
+  count = var.is_peerring_required ? 1 : 0
+  route_table_id            = data.aws_route_table.default.id
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.dev_def[count.index].id
+}
