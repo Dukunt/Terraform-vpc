75740208ec032db0d6e0c74c14b43b144ad73f6a
sateesh <sateesh1417@gmail.com>
2026-03-09 19:43:03 +0000

    az

---
+++ peering.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc_peering_connection" "dev_def" {
+  #peer_owner_id = var.peer_owner_id
+  count = var.is_peerring_required ? 1 : 0
+
+  #Acceptor(Target)
+  peer_vpc_id   = aws_default_vpc.default.id
+
+  #Requestor
+  vpc_id        = aws_vpc.roboshop.id
+  auto_accept = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+  tags = merge (
+                local.common_tags,
+            { Name = "${var.project}-${var.environment}-default" }
+  )
+
+}
