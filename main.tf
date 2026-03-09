4f32a638bebd1c6f2fe45dc0b3d63f9d48f57d5d
sateesh <sateesh1417@gmail.com>
2026-03-09 18:20:28 +0000

    az

--- main.tf
+++ main.tf
@@ -144,3 +144,21 @@ resource "aws_route" "database" {
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_nat_gateway.nat.id
 }
+
+resource "aws_route_table_association" "public_rta" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_rta" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "database_rta" {
+  count = length(var.database_subnet_cidrs)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database_rt.id
+}
